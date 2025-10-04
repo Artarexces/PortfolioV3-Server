6ca6c81e386a8dc96243a83ea117cb78b2c313e7
Martin Rodriguez <martinrodriguezdev96@gmail.com>
2025-10-04 19:29:20 -0300

    add: interface & schema model

---
+++ src/model/User.ts
@@ -0,0 +1,15 @@
+import { Schema, model } from 'mongoose'
+
+interface IUser {
+    name: string;
+    email: string;
+    password: string;
+}
+
+const UserSchema = new Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: false},
+    password: {type: String, required: true}
+})
+
+export const UserModel = model<IUser>('User', UserSchema)
